923ced9224b9ae391ebc80aa253cb009c24dbbe1
Andrew Bell <andrew.bell@kyndryl.com>
2025-07-17 10:24:50 +0100

    added test python code

---
+++ test_index.py
@@ -0,0 +1,23 @@
+import pytest
+
+from index import add, minus, divide, multiply
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(14, 15) == 29
+    with pytest.raises(TypeError):
+        add(4, 'Three-Thousand')
+
+def test_minus():
+    assert minus(5, 3) == 2
+    assert minus(6, 9) == -3
+    assert minus(11, 4) != 4
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(5, 2) == 2.5
+    with pytest.raises(ZeroDivisionError):
+        assert divide(4, 0)
+
+def test_multiply():
+    assert multiply(3, 6) == 18
